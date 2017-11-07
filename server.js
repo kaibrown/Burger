cba24b88d336660b64e69898d36ee6e4863ff041
Kaisey Brown <kaiseybrown@Kaiseys-MacBook-Air.local>
2017-11-07 10:59:30 -0500

    "building"

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static( __dirname + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+var port = 3000;
+app.listen(port);
