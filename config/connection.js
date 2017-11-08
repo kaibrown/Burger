7c8b8c1341848735aef8b5cd08f4df09408752ab
Kaisey Brown <kaiseybrown@Kaiseys-MacBook-Air.local>
2017-11-07 23:06:51 -0500

    deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,29 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 8889,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+var connection;
+
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 8889,
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "burgers_db",
+    });
+};
+
+// var connection = mysql.createConnection({
+//   port: 8889,
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
